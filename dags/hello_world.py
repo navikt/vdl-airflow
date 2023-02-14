76a92cf73b10ccb919c24ece41edef107c104099
Patrick Pedersen <patrick.pedersen@nav.no>
2023-02-14 17:07:39 +0100

    feat: test default success message to slack

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -7,12 +7,13 @@ from operators.slack_operator import slack_info, slack_error
 @dag(
     start_date=datetime(2023, 2, 14),
     schedule_interval=None,
+    on_success_callback=slack_info,
     on_failure_callback=slack_error,
 )
 def hello_world():
-    @task()
+    @task(on_success_callback=slack_info)
     def send_slack_message():
-        slack_info("Hello, World!", channel="#virksomhetsdatalaget-info")
+        slack_info(message="Hello, World!", channel="#virksomhetsdatalaget-info")
 
     send_slack_message()
 
