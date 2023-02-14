0a6b6064f53e71e6a9abb1084ce40645f2e04dee
Patrick Pedersen <patrick.pedersen@nav.no>
2023-02-14 17:03:43 +0100

    feat: test automatic failmessage to slack

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -1,10 +1,14 @@
 from datetime import datetime
 from airflow.decorators import dag, task
 
-from operators.slack_operator import slack_info
+from operators.slack_operator import slack_info, slack_error
 
 
-@dag(start_date=datetime(2023, 2, 14), schedule_interval=None)
+@dag(
+    start_date=datetime(2023, 2, 14),
+    schedule_interval=None,
+    on_failure_callback=slack_error,
+)
 def hello_world():
     @task()
     def send_slack_message():
