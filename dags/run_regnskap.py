1b5a5795c1a1822e15a7a7f072ca0434bd045ea7
Patrick Pedersen <patrick.pedersen@nav.no>
2023-02-28 15:38:55 +0100

    Prøv pub ingress for å nå applikasjoner i fss-sonen

--- dags/run_regnskap.py
+++ dags/run_regnskap.py
@@ -4,6 +4,8 @@ from airflow.decorators import dag, task
 
 from operators.slack_operator import slack_error, slack_info
 
+URL = "https://vdl-regnskap.dev-fss-pub.nais.io"
+
 
 @dag(
     start_date=datetime(2023, 2, 28),
@@ -12,19 +14,20 @@ from operators.slack_operator import slack_error, slack_info
     on_failure_callback=slack_error,
 )
 def run_regnskap():
-    
     @task()
     def send_slack_message():
         slack_info(message="Jeg kjører ingest LoL!")
-    
+
     @task()
     def ingest_dimensional_data() -> None:
         import requests
-        res = requests.get(url="https://vdl-regnskap.dev.intern.nav.no/inbound/run/dimensional_data")
-        
-    send_slack_message()
-    ingest_dimensional_data()
+
+        res = requests.get(
+            url="https://vdl-regnskap.dev-fss-pub.nais.io/inbound/run/dimensional_data"
+        )
+
+    slack_message = send_slack_message()
+    ingest = ingest_dimensional_data()
+
 
 run_regnskap()
-
-
