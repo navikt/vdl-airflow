4878afbf51c0da7ce0c323ad05e7a0374033e77f
Patrick Pedersen <patrick.pedersen@nav.no>
2023-04-13 10:40:58 +0200

    test

--- dags/run_regnskap.py
+++ dags/run_regnskap.py
@@ -2,8 +2,9 @@ from datetime import datetime
 
 from airflow.models import Variable
 from airflow.decorators import dag, task
-from airflow.providers.http.sensors.http import HttpSensor
 from operators.slack_operator import slack_error, slack_info
+from airflow.sensors.base import PokeReturnValue
+
 
 URL = Variable.get("VDL_REGNSKAP_URL")
 
@@ -13,6 +14,7 @@ URL = Variable.get("VDL_REGNSKAP_URL")
     schedule_interval=None,
     on_success_callback=slack_info,
     on_failure_callback=slack_error,
+    default_args={"retries": 1},
 )
 def run_regnskap():
     @task()
@@ -20,31 +22,33 @@ def run_regnskap():
         slack_info(message="Jeg kjører ingest LoL!")
 
     @task()
-    def ingest_dimensional_data():
+    def ingest_dimensional_data() -> dict:
         import requests
 
-        return requests.get(url=f"{URL}/inbound/run/dimensional_data")
-
-    def response_check(response, task_instance):
-        # The task_instance is injected, so you can pull data form xcom
-        # Other context variables such as dag, ds, execution_date are also available.
-        xcom_data = task_instance.xcom_pull(task_ids="ingest_dimensional_data")
-        # In practice you would do something more sensible with this data..
-        print(xcom_data)
-        print(response)
-        return True
+        return requests.get(url=f"{URL}/inbound/run/dimensional_data").json()
 
     job_id = ingest_dimensional_data()
 
-    sensor = HttpSensor(
-        task_id="my_http_sensor",
-        http_conn_id="vdl-regnskap",
-        endpoint=f"inbound/status/{job_id.job_id}",
-        method="GET",
-        request_params=dict(job_id=job_id),
-        response_check=response_check,
-    )
+    @task.sensor(poke_interval=60, timeout=3600, mode="reschedule")
+    def wait_for_upstream(job_id: dict) -> PokeReturnValue:
+        import requests
 
+        id = job_id.get("job_id")
+        response: dict = requests.get(url=f"{URL}/inbound/status/{id}").json()
+        print(response)
+        job_status = response.get("status")
+        print(job_status)
+        if job_status is "running":
+            print("inside running")
+            return PokeReturnValue(is_done=False)
+        if job_status is "done":
+            print("inside done")
+            return PokeReturnValue(is_done=True)
+        if job_status is "error":
+            print("inside error")
+            raise Exception("Lastejobben har feilet! Sjekk loggene til podden")
+
+    wait = wait_for_upstream(job_id)
     # @task()
     # def ingest_ledger_open():
     #    import requests
@@ -62,7 +66,7 @@ def run_regnskap():
     # ledger_closed = ingest_ledger_closed()
     # ledger_open = ingest_ledger_open()
 
-    slack_message >> job_id >> sensor
+    slack_message >> job_id >> wait
     # slack_message >> dimensional_data
     # slack_message >> ledger_closed
     # slack_message >> ledger_open
