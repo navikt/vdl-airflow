52244116dd48451c323654850557e5b4bba5d156
Patrick Pedersen <patrick.pedersen@nav.no>
2023-04-17 14:25:18 +0200

    Kjøre dbt etter ingest fra oebs i run_regnskap

--- dags/run_regnskap.py
+++ dags/run_regnskap.py
@@ -83,6 +83,35 @@ def run_regnskap():
 
     slack_message = send_slack_message()
 
+    @task()
+    def run_dbt_job(job: str) -> dict:
+        import requests
+
+        return requests.get(url=f"{URL}/dbt/{job}").json()
+
+    dbt_run = run_dbt_job.override(task_id="dbt_run")("run")
+    dbt_test = run_dbt_job.override(task_id="dbt_test")("test")
+
+    @task.sensor(poke_interval=60, timeout=2 * 60 * 60, mode="reschedule")
+    def wait_for_dbt(job_status: dict) -> PokeReturnValue:
+        import requests
+
+        id = job_status.get("job_id")
+
+        response: dict = requests.get(url=f"{URL}/dbt/status/{id}").json()
+        print(response)
+        job_status = response.get("status")
+
+        if job_status == "running":
+            return PokeReturnValue(is_done=False)
+        if job_status == "done":
+            return PokeReturnValue(is_done=True)
+        if job_status == "error":
+            raise Exception("Lastejobben har feilet! Sjekk loggene til podden")
+
+    wait_dbt_run = wait_for_dbt.override(task_id="wait_for_dbt_run")(dbt_run)
+    wait_dbt_test = wait_for_dbt.override(task_id="wait_for_dbt_test")(dbt_test)
+
     slack_message >> dimensonal_data >> wait_dimensonal_data
     slack_message >> sync_check >> wait_sync_check
     slack_message >> general_ledger_open >> wait_general_ledger_open
@@ -91,6 +120,17 @@ def run_regnskap():
     slack_message >> balance_open >> wait_balance_open
     slack_message >> balance_budget >> wait_balance_budget
     slack_message >> balance_closed >> wait_balance_closed
+
+    wait_dimensonal_data >> dbt_run
+    wait_sync_check >> dbt_run
+    wait_general_ledger_open >> dbt_run
+    wait_general_ledger_budget >> dbt_run
+    wait_general_ledger_closed >> dbt_run
+    wait_balance_open >> dbt_run
+    wait_balance_budget >> dbt_run
+    wait_balance_closed >> dbt_run
+
+    dbt_run >> wait_dbt_run >> dbt_test >> wait_dbt_test
 
 
 run_regnskap()
