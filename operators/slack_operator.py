bc0c22ecb08a0db52bc60e60cfd22a0e0bb6e164
Patrick Pedersen <patrick.pedersen@nav.no>
2023-05-05 11:50:30 +0200

    Make it possible to expand default message from slack operators

--- operators/slack_operator.py
+++ operators/slack_operator.py
@@ -11,17 +11,19 @@ def slack_info(
 ):
     if channel is None:
         channel = Variable.get("slack_info_channel")
-    if message is None:
-        message = f"Airflow DAG: {context['dag'].dag_id} har kjørt ferdig."
-    __slack_message(context, message, channel, emoji)
+    info_message = f"Airflow DAG: {context['dag'].dag_id} har kjørt ferdig."
+    if message:
+        info_message = f"{info_message}\n\n{message}"
+    __slack_message(context, info_message, channel, emoji)
 
 
 def slack_error(context=None, message=None, channel: str = None, emoji=":red_circle:"):
     if channel is None:
         channel = Variable.get("slack_error_channel")
-    if message is None:
-        message = f"En Airflow DAG feilet!\n\n- DAG: {context['dag'].dag_id}\n- Task: {context['task_instance'].task_id}"
-    __slack_message(context, message, channel, emoji)
+    error_message = f"En Airflow DAG feilet!\n\n- DAG: {context['dag'].dag_id}\n- Task: {context['task_instance'].task_id}"
+    if message:
+        error_message = f"{error_message}\n- Melding: {message}"
+    __slack_message(context, error_message, channel, emoji)
 
 
 def __slack_message(
