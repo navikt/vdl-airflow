a1db765655be76059d6ea7324dec1d4cbe036556
Patrick Pedersen <patrick.pedersen@nav.no>
2023-05-09 13:00:01 +0200

    Fix context for slack_operator

--- operators/slack_operator.py
+++ operators/slack_operator.py
@@ -11,30 +11,29 @@ def slack_info(
 ):
     if channel is None:
         channel = Variable.get("slack_info_channel")
+    if context is None:
+        context = get_current_context()
     info_message = f"Airflow DAG: {context['dag'].dag_id} har kjørt ferdig."
     if message:
         info_message = f"{info_message}\n\n{message}"
-    __slack_message(context, info_message, channel, emoji)
+    __slack_message(info_message, channel)
 
 
 def slack_error(context=None, message=None, channel: str = None, emoji=":red_circle:"):
     if channel is None:
         channel = Variable.get("slack_error_channel")
+    if context is None:
+        context = get_current_context()
     error_message = f"En Airflow DAG feilet!\n\n- DAG: {context['dag'].dag_id}\n- Task: {context['task_instance'].task_id}"
     if message:
         error_message = f"{error_message}\n- Melding: {message}"
-    __slack_message(context, error_message, channel, emoji)
+    __slack_message(error_message, channel)
 
 
 def __slack_message(
-    context: str,
     message: str,
     channel: str,
-    emoji: str,
-    attachments: Optional[list] = None,
 ):
-    if context is None:
-        context = get_current_context()
     SlackAPIPostOperator(
         task_id="slack-message",
         channel=channel,
