cac34dc7cf80c2edafe715c8caa5617ed21a686a
Patrick Pedersen <patrick.pedersen@nav.no>
2023-07-19 16:12:46 +0200

    Opprette snowflake con for anaplan_regnskaphierarkier

---
+++ dags/anaplan_regnskaphierarkier.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+from airflow.decorators import dag, task
+
+from operators.slack_operator import slack_error, slack_success, slack_info
+
+
+@dag(
+    start_date=datetime(2023, 7, 17),
+    schedule_interval=None,
+    on_success_callback=slack_success,
+    on_failure_callback=slack_error,
+)
+def anaplan_regnskaphierarkier():
+    @task
+    def transfer():
+        from anaplan.regnskaphierarki.singleChunkUpload import transfer_data
+        from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+
+        with SnowflakeHook().get_cursor() as cursor:
+            cursor.execute("select 1 from dual")
+            result = cursor.fetchone()
+            print(result)
+
+    upload = transfer()
+
+    @task
+    def update_hierarchy_data():
+        from anaplan.regnskaphierarki.import_hierarchy import hierarchy_data
+
+    refresh_hierarchy_data = update_hierarchy_data()
+
+    @task
+    def update_module_data():
+        from anaplan.regnskaphierarki.import_module_data import module_data
+
+    refresh_module_data = update_module_data()
+
+    upload >> refresh_hierarchy_data >> refresh_module_data
+
+
+anaplan_regnskaphierarkier()
