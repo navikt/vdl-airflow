ccfe2e827ac73397ea0046fa595f26170944839e
Patrick Pedersen <patrick.pedersen@nav.no>
2023-07-21 09:46:35 +0200

    Filtrere ut artskontier uten nivå 1

--- anaplan/regnskaphierarki/get_data.py
+++ anaplan/regnskaphierarki/get_data.py
@@ -6,7 +6,13 @@ from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 def get_artskonti_data():
     with SnowflakeHook().get_cursor() as cursor:
         cursor.execute(
-            "select * from reporting.microstrategy.dim_artskonti where er_budsjetterbar = 1"
+            """
+            select *
+            from reporting.microstrategy.dim_artskonti
+            where
+                er_budsjetterbar = 1 and
+                artskonti_segment_kode_niva_1 is not null
+            """
         )
         column_names = map(lambda x: x[0], cursor.description)
         result = cursor.fetchall()
