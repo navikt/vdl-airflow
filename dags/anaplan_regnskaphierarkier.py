4dfc51ca31c7f38cedb21b32db9b476ed0b40a26
Ruby Paloma <ruby.chinyere.paloma@nav.no>
2023-07-24 13:23:05 +0200

    Importerer dim_felles data til Anaplan

--- dags/anaplan_regnskaphierarkier.py
+++ dags/anaplan_regnskaphierarkier.py
@@ -15,15 +15,12 @@ from custom.operators.slack_operator import slack_error, slack_success, slack_in
     on_success_callback=slack_success,
     on_failure_callback=slack_error,
 )
-
-
 def anaplan_regnskaphierarkier():
-    
     wGuid = "8a868cda860a533a0186334e91805794"
     mGuid = "A07AB2A8DBA24E13B8A6E9EBCDB6235E"
     username = "virksomhetsdatalaget@nav.no"
     password = Variable.get("anaplan_password")
-    
+
     @task
     def transfer():
         from anaplan.singleChunkUpload import transfer_data
@@ -43,7 +40,7 @@ def anaplan_regnskaphierarkier():
         }
 
         with SnowflakeHook().get_cursor() as cursor:
-            query =  """
+            query = """
                 select *
                 from reporting.microstrategy.dim_artskonti
                 where
@@ -59,7 +56,7 @@ def anaplan_regnskaphierarkier():
     @task
     def update_hierarchy_data():
         from anaplan.import_data import import_data
-        
+
         importData = {
             "id": "112000000052",
             "name": "Test Artskonto Flat from dim_artskonti.csv",
@@ -76,16 +73,15 @@ def anaplan_regnskaphierarkier():
         from anaplan.import_data import import_data
 
         importData = {
-            "id" : "112000000051",
-            "name" : "TEST 01.02 Test Kontostruktur 2 from dim_artskonti.csv",
-            "importDataSourceId" : "113000000033",
-            "importType" : "MODULE_DATA"
+            "id": "112000000051",
+            "name": "TEST 01.02 Test Kontostruktur 2 from dim_artskonti.csv",
+            "importDataSourceId": "113000000033",
+            "importType": "MODULE_DATA",
         }
 
         import_data(wGuid, mGuid, username, password, importData)
 
     refresh_module_data = update_module_data()
-
 
     @task
     def transfer_felles():
@@ -94,19 +90,19 @@ def anaplan_regnskaphierarkier():
         from anaplan.get_data import get_data
 
         fileData = {
-            "id" : "113000000034",
-            "name" : "dim_felles.csv",
-            "chunkCount" : 1,
-            "delimiter" : "\"",
-            "encoding" : "UTF-8",
-            "firstDataRow" : 2,
-            "format" : "txt",
-            "headerRow" : 1,
-            "separator" : ","
+            "id": "113000000034",
+            "name": "dim_felles.csv",
+            "chunkCount": 1,
+            "delimiter": '"',
+            "encoding": "UTF-8",
+            "firstDataRow": 2,
+            "format": "txt",
+            "headerRow": 1,
+            "separator": ",",
         }
 
         with SnowflakeHook().get_cursor() as cursor:
-            query =  """
+            query = """
                 select *
                 from reporting.microstrategy.dim_felles
                 where
@@ -118,8 +114,38 @@ def anaplan_regnskaphierarkier():
 
     upload_felles = transfer_felles()
 
+    @task
+    def update_hierarchy_data_felles():
+        from anaplan.import_data import import_data
+
+        importData = {
+            "id": "112000000053",
+            "name": "Test Felles Flat from dim_felles.csv",
+            "importDataSourceId": "113000000034",
+            "importType": "HIERARCHY_DATA",
+        }
+
+        import_data(wGuid, mGuid, username, password, importData)
+
+    refresh_hierarchy_data_felles = update_hierarchy_data_felles()
+
+    @task
+    def update_module_data_felles():
+        from anaplan.import_data import import_data
+
+        importData = {
+            "id": "112000000054",
+            "name": "TEST 01.02 Test Felles from dim_felles.csv",
+            "importDataSourceId": "113000000034",
+            "importType": "MODULE_DATA",
+        }
+
+        import_data(wGuid, mGuid, username, password, importData)
+
+    refresh_module_data_felles = update_module_data_felles()
+
     upload >> refresh_hierarchy_data >> refresh_module_data
-    upload_felles
+    upload_felles >> refresh_hierarchy_data_felles >> refresh_module_data_felles
 
 
 anaplan_regnskaphierarkier()
