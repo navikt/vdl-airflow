30f531faea326fd3353b32a9f8f7893d3c193f16
Ruby Paloma <ruby.chinyere.paloma@nav.no>
2023-07-31 14:36:13 +0200

    Refakturerer hr-data til Anaplan

--- dags/anaplan_ansattdata.py
+++ dags/anaplan_ansattdata.py
@@ -34,7 +34,7 @@ def anaplan_ansattdata():
             result = cursor.fetchall()
             print(f"Number of rows: {len(result)}")
             f = StringIO(newline="")
-            writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+            writer = csv.writer(f)
             writer.writerow(column_names)
             writer.writerows(result)
             return f.getvalue()
