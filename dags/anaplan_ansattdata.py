18f91b49b91035dff4d9fc6f3823201f32959f00
Ruby Paloma <ruby.chinyere.paloma@nav.no>
2023-08-01 15:28:14 +0200

    Refakturerer ansattdata til Anaplan

--- dags/anaplan_ansattdata.py
+++ dags/anaplan_ansattdata.py
@@ -58,15 +58,15 @@ def anaplan_ansattdata():
 
     refresh_hierarchy_data = update_data.override(task_id="update_hierarchy_hr_data")(
         importData={
-            "id": "112000000072",
-            "name": "Test Ansatte Fla from anaplan_hrres_stillinger.csv",
+            "id": "112000000074",
+            "name": "Test Ansatte Flat 2 from anaplan_hrres_stillinger.csv",
         }
     )
 
     refresh_module_data = update_data.override(task_id="update_module_hr_data")(
         importData={
-            "id": "112000000073",
-            "name": "TEST 01.07 HR-Data - Copy from anaplan_hrres_stillinger.csv",
+            "id": "112000000075",
+            "name": "TEST 01.07 HR-Data 2 from anaplan_hrres_stillinger.csv",
         }
     )
 
