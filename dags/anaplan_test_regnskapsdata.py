8ec816242cb18f7507193b99fc0046d565f54c97
Patrick Pedersen <patrick.pedersen@nav.no>
2023-08-07 13:29:06 +0200

    Slå sammen tasks i anaplan_test_regnskapsdata

--- dags/anaplan_test_regnskapsdata.py
+++ dags/anaplan_test_regnskapsdata.py
@@ -22,21 +22,23 @@ def anaplan_test_regnskapsdata():
     password = Variable.get("anaplan_password")
 
     @task
-    def transfer(fileData: dict, query: str):
+    def transfer(
+        fileData: dict,
+        query: str,
+        import_hierarchy_data: dict,
+        import_module_data: dict,
+    ):
         from anaplan.singleChunkUpload import transfer_data
         from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
         from anaplan.get_data import get_data
+        from anaplan.import_data import import_data
 
         with SnowflakeHook().get_cursor() as cursor:
             data = get_data(query, cursor)
 
         transfer_data(wGuid, mGuid, username, password, fileData, data)
-
-    @task
-    def update_data(importData: dict):
-        from anaplan.import_data import import_data
-
-        import_data(wGuid, mGuid, username, password, importData)
+        import_data(wGuid, mGuid, username, password, import_hierarchy_data)
+        import_data(wGuid, mGuid, username, password, import_module_data)
 
     upload = transfer.override(task_id="transfer_regnskapsdata")(
         fileData={
@@ -44,52 +46,44 @@ def anaplan_test_regnskapsdata():
             "name": "agg_hovedbok_posteringer_all_mnd_snowflake.csv",
         },
         query="""
-                select
-    md5(
-        periode_navn||
-        statsregnskapskonti_segment_kode||
-        artskonti_segment_kode||
-        kostnadssteder_segment_kode||
-        produkter_segment_kode||
-        oppgaver_segment_kode||
-        felles_segment_kode
-    ) as pk,
-    periode_navn,
-    statsregnskapskonti_segment_kode,
-    artskonti_segment_kode,
-    kostnadssteder_segment_kode,
-    produkter_segment_kode,
-    oppgaver_segment_kode,
-    felles_segment_kode,
-    sum(sum_netto_nok) as sum_netto_nok
-from reporting.microstrategy.agg_hovedbok_posteringer_all_mnd
-where
-    ER_BUDSJETT_POSTERING = 0 and
-    hovedbok_id = '3022' and
-    endswith(periode_navn, '23') and
-    endswith(artskonti_segment_kode, '0000000') and
-    endswith(statsregnskapskonti_segment_kode, '000000')
-group by 1,2,3,4,5,6,7,8
-                """,
-    )
-
-    refresh_hierarchy_data = update_data.override(
-        task_id="update_hierarchy_regnskapsdata"
-    )(
-        importData={
+            select
+                md5(
+                    periode_navn||
+                    statsregnskapskonti_segment_kode||
+                    artskonti_segment_kode||
+                    kostnadssteder_segment_kode||
+                    produkter_segment_kode||
+                    oppgaver_segment_kode||
+                    felles_segment_kode
+                ) as pk,
+                periode_navn,
+                statsregnskapskonti_segment_kode,
+                artskonti_segment_kode,
+                kostnadssteder_segment_kode,
+                produkter_segment_kode,
+                oppgaver_segment_kode,
+                felles_segment_kode,
+                sum(sum_netto_nok) as sum_netto_nok
+            from reporting.microstrategy.agg_hovedbok_posteringer_all_mnd
+            where
+                ER_BUDSJETT_POSTERING = 0 and
+                hovedbok_id = '3022' and
+                endswith(periode_navn, '23') and
+                endswith(artskonti_segment_kode, '0000000') and
+                endswith(statsregnskapskonti_segment_kode, '000000')
+            group by 1,2,3,4,5,6,7,8
+        """,
+        import_hierarchy_data={
             "id": "112000000088",
             "name": "Test Regnskap Flat 2 from agg_hovedbok_posteringer_all_mnd_s",
-        }
-    )
-
-    refresh_module_data = update_data.override(task_id="update_module_regnskapsdata")(
-        importData={
+        },
+        import_module_data={
             "id": "112000000089",
             "name": "TEST 01.08 Regnskap 2 from agg_hovedbok_posteringer_all_mnd~",
-        }
+        },
     )
 
-    (upload >> refresh_hierarchy_data >> refresh_module_data)
+    (upload)
 
 
 anaplan_test_regnskapsdata()
