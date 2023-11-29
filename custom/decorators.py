f2df61ce370a4e24c20eafaf172a717b665cf524
Patrick Pedersen <patrick.pedersen@nav.no>
2023-11-29 15:06:50 +0100

    Fikse "TypeError: task() missing 1 required positional argument: 'func'"

--- custom/decorators.py
+++ custom/decorators.py
@@ -5,28 +5,31 @@ from airflow.decorators import task as airflow_task
 from kubernetes import client as k8s
 
 
-def task(func: Callable, allowlist: list[str] = ["slack.com"], **kwargs):
-    @airflow_task(
-        executor_config={
-            "pod_override": k8s.V1Pod(
-                metadata=k8s.V1ObjectMeta(
-                    annotations={"allowlist": ",".join(allowlist)}
-                ),
-                spec=k8s.V1PodSpec(
-                    containers=[
-                        k8s.V1Container(
-                            name="base",
-                            image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow@sha256:5edb4e907c93ee521f5f743c3b4346b1bae26721820a2f7e8dfbf464bf4c82ba",
-                        )
-                    ]
-                ),
-            )
-        },
-        **kwargs
-    )
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        value = func(*args, **kwargs)
-        return value
+def task(allowlist: list[str] = ["slack.com"], **kwargs):
+    def decorator(func):
+        @airflow_task(
+            executor_config={
+                "pod_override": k8s.V1Pod(
+                    metadata=k8s.V1ObjectMeta(
+                        annotations={"allowlist": ",".join(allowlist)}
+                    ),
+                    spec=k8s.V1PodSpec(
+                        containers=[
+                            k8s.V1Container(
+                                name="base",
+                                image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow@sha256:5edb4e907c93ee521f5f743c3b4346b1bae26721820a2f7e8dfbf464bf4c82ba",
+                            )
+                        ]
+                    ),
+                )
+            },
+            **kwargs
+        )
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            value = func(*args, **kwargs)
+            return value
 
-    return wrapper
+        return wrapper
+
+    return decorator
