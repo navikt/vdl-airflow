e3820cca2baf7f3ee830dfe5b8d646b349c0e3b4
Patrick Pedersen <patrick.pedersen@nav.no>
2024-03-19 18:02:00 +0100

    Endre run_regnskap__ap kjøretid til 19:00 UTC

--- dags/run_regnskap__ap.py
+++ dags/run_regnskap__ap.py
@@ -15,7 +15,7 @@ URL = Variable.get("VDL_REGNSKAP_URL")
 
 with DAG(
     start_date=datetime(2023, 3, 18),
-    schedule_interval="0 20 * * *",
+    schedule_interval="0 19 * * *",
     dag_id="regnskap__ap_dag",
     catchup=False,
     default_args={"on_failure_callback": slack_error, "retries": 3},
