2e711976596f4b0ced8d4058c1262e94865c12d2
trineno <161321609+trineno@users.noreply.github.com>
2024-05-15 14:57:43 +0200

    Legge til task sensor for eyeshare og anaplan logs

--- dags/run_audit_logs.py
+++ dags/run_audit_logs.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.exceptions import AirflowFailException
 from airflow.models import Variable
+from airflow.sensors.base import PokeReturnValue
 from kubernetes import client as k8s
 
 from custom.operators.slack_operator import slack_error
@@ -56,16 +57,70 @@ def run_audit_logs():
             )
         return response.json()
     
+    @task.sensor(
+        poke_interval=60,
+        timeout=8 * 60 * 60,
+        mode="reschedule",
+        executor_config={
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(
+                    annotations={
+                        "allowlist": ",".join(
+                            [
+                                "slack.com",
+                                "vdl-loggdata.intern.nav.no",
+                            ]
+                        )
+                    }
+                ),
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image=CUSTOM_IMAGE,
+                            resources=k8s.V1ResourceRequirements(
+                                requests={"ephemeral-storage": "100M"},
+                                limits={"ephemeral-storage": "200M"},
+                            ),
+                        )
+                    ]
+                ),
+            )
+        },
+    )
+    def check_status_for_inbound_job(job_id: dict) -> PokeReturnValue:
+        import requests
+
+        id = job_id.get("job_id")
+
+        response: requests.Response = requests.get(url=f"{URL}/inbound/status/{id}")
+        if response.status_code > 400:
+            raise AirflowFailException(
+                "inboundjobb eksisterer mest sannsynlig ikke på podden"
+            )
+        response: dict = response.json()
+        print(response)
+        job_status = response.get("status")
+        if job_status == "done":
+            return PokeReturnValue(is_done=True)
+        if job_status == "error":
+            error_message = response["job_result"]["error_message"]
+            raise AirflowFailException(
+                f"Lastejobben har feilet! Sjekk loggene til podden. Feilmelding: {error_message}"
+            )
+
     eyeshare = run_inbound_job.override(task_id="start_eyeshare")(
         "eyeshare"
     )
+    wait_eyeshare = check_status_for_inbound_job(eyeshare)
 
     anaplan = run_inbound_job.override(task_id="start_anaplan")(
         "anaplan"
     )
+    wait_anaplan = check_status_for_inbound_job(anaplan)
 
-    eyeshare
+    eyeshare >> wait_eyeshare
     
-    anaplan
+    anaplan >> wait_anaplan
 
 run_audit_logs()
