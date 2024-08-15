e81263a6ff78f1a391419722f6a99a04874921d9
trineno <161321609+trineno@users.noreply.github.com>
2024-08-15 14:26:57 +0200

    Oppdatere inbound-image

--- dags/test_dataverk.py
+++ dags/test_dataverk.py
@@ -10,7 +10,7 @@ with DAG("test_dataverk", start_date=days_ago(1), schedule_interval=None) as dag
         repo="navikt/vdl-eiendom",
         branch="teste-dataverk-operators",
         script_path="test_airflow.py",
-        image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow-inbound@sha256:df16552fbba6affd60fc3c47ca3a1705f4ca5fa39bd396fe5393395a2dcd17e1",
+        image="europe-north1-docker.pkg.dev/nais-management-233d/virksomhetsdatalaget/vdl-airflow-inbound@sha256:e3ea3898de084a1800915d40b2adde6ea53bdb6b996aa91763b0e13122f1ffae",
         extra_envs={
             "EIENDOM_RAW_DB": Variable.get("EIENDOM_RAW_DB"),
             "MAINMANAGER_API_USERNAME": Variable.get("MAINMANAGER_API_USERNAME"),
