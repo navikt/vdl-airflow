e31e335f7fa94c51f317ba9a9b0c2bfdb3f9876e
Patrick Pedersen <patrick.pedersen@nav.no>
2024-08-19 16:19:15 +0200

    Teste default_args

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -5,6 +5,7 @@ from airflow.decorators import dag, task
 # from custom.decorators import task
 from kubernetes import client as k8s
 
+from custom.decorators import CUSTOM_IMAGE
 from custom.operators.slack_operator import (
     slack_error,
     slack_info,
@@ -16,12 +17,24 @@ from custom.operators.slack_operator import (
 @dag(
     start_date=datetime(2023, 2, 14),
     schedule_interval=None,
-    on_success_callback=test_slack,
-    on_failure_callback=test_slack,
+    on_success_callback=slack_success_old,
+    on_failure_callback=slack_error,
     default_args={
         "executor_config": {
             "pod_override": k8s.V1Pod(
-                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"})
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"}),
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image=CUSTOM_IMAGE,
+                            resources=k8s.V1ResourceRequirements(
+                                requests={"ephemeral-storage": "100M"},
+                                limits={"ephemeral-storage": "200M"},
+                            ),
+                        )
+                    ]
+                ),
             )
         },
     },
