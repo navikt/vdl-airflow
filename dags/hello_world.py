e648ecd481912a480b96316c49078d12032cf732
Patrick Pedersen <patrick.pedersen@nav.no>
2024-09-13 14:59:09 +0200

    Trigge hello_world_2 når datasettet hello_world er oppdatert

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -47,3 +47,38 @@ def hello_world():
 
 
 hello_world()
+
+
+@dag(
+    start_date=datetime(2023, 2, 14),
+    schedule_interval=None,
+    schedule=[Dataset("hello_world")],
+    on_success_callback=test_slack,
+    on_failure_callback=slack_error,
+    default_args={
+        "executor_config": {
+            "pod_override": k8s.V1Pod(
+                metadata=k8s.V1ObjectMeta(annotations={"allowlist": "slack.com"}),
+                spec=k8s.V1PodSpec(
+                    containers=[
+                        k8s.V1Container(
+                            name="base",
+                            image=CUSTOM_IMAGE,
+                        )
+                    ]
+                ),
+            )
+        },
+    },
+)
+def hello_world_2():
+    @task()
+    def send_slack_message():
+        slack_info(message="Hello, Again!")
+
+    slack_message = send_slack_message()
+
+    slack_message
+
+
+hello_world_2()
