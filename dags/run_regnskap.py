390db9a2b241ad1fec61346bf9488af2c0068822
trineno <161321609+trineno@users.noreply.github.com>
2024-11-22 14:37:42 +0100

    Refaktorere elementary-kode i run regnskap

--- dags/run_regnskap.py
+++ dags/run_regnskap.py
@@ -301,17 +301,11 @@ with DAG(
     regnskap_report = elementary_operator(
         dag=dag,
         task_id="regnskap_report",
-        commands=["./run.sh", "report"],
-        allowlist=[
-            "slack.com",
-            "files.slack.com",
-            "wx23413.europe-west4.gcp.snowflakecomputing.com",
-        ],
-        extra_envs={
-            "DB": "regnskap",
-            "DB_ROLE": "regnskap_transformer",
-            "DB_WH": "regnskap_transformer",
-        },
+        commands=["report"],
+        database="regnskap",
+        schema="meta",
+        snowflake_role="regnskap_transformer",
+        snowflake_warehouse="regnskap_transformer",
     )
 
     period_status >> wait_period_status
