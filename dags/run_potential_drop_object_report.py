943912ce66d5997553ce8d142516a2037eb1c9b5
Patrick Pedersen <patrick.pedersen@nav.no>
2025-04-09 15:31:11 +0200

    Sett snowflake konfigurasjon i run_potential_drop_object_report

--- dags/run_potential_drop_object_report.py
+++ dags/run_potential_drop_object_report.py
@@ -20,6 +20,11 @@ with DAG(
         requirements_path="waste_report/requirements.txt",
         slack_channel=Variable.get("slack_error_channel"),
         do_xcom_push=True,
+        extra_envs={
+            "SNOWFLAKE_USER": Variable.get("srv_snowflake_user"),
+            "SNOWFLAKE_PASSWORD": Variable.get("srv_snowflake_password"),
+            "SNOWFLAKE_AUTHENTICATOR": "snowflake",
+        },
     )
 
     # notify_slack_success = slack_success(dag=dag)
