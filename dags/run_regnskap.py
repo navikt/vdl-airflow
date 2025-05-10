8295c036c5975aa064f7aad0b5cc5dc3b5cc55e6
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2025-05-10 07:03:21 +0200

    kommentert vekk tasks

--- dags/run_regnskap.py
+++ dags/run_regnskap.py
@@ -313,20 +313,20 @@ with DAG(
 
     sync_check >> wait_sync_check
 
-    suppliers >> wait_suppliers
-    segment >> wait_segment
-    hierarchy >> wait_hierarchy
+    # suppliers >> wait_suppliers
+    # segment >> wait_segment
+    # hierarchy >> wait_hierarchy
     budget >> wait_budget
     prognosis >> wait_prognosis
-    customers >> wait_customers
+    # customers >> wait_customers
 
     wait_sync_check >> dbt_freshness
-    wait_suppliers >> dbt_freshness
-    wait_hierarchy >> dbt_freshness
-    wait_segment >> dbt_freshness
+    # wait_suppliers >> dbt_freshness
+    # wait_hierarchy >> dbt_freshness
+    # wait_segment >> dbt_freshness
     wait_budget >> dbt_freshness
     wait_prognosis >> dbt_freshness
-    wait_customers >> dbt_freshness
+    # wait_customers >> dbt_freshness
 
     dbt_freshness >> wait_dbt_freshness >> dbt_run >> wait_dbt_run >> slack_summary
     wait_dbt_run >> regnskap_report
